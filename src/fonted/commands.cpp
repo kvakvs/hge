b280c356332d804f8aba59adb9ff2e9f6b286d45
dmytro lytovchenko <dmytro.lytovchenko@gmail.com>
2021-02-17 17:25:23 +0100

    Version bump to 1.9 (possible minor API changes)

--- src/fonted/commands.cpp
+++ src/fonted/commands.cpp
@@ -1,5 +1,5 @@
 /*-----------------------------------------------------------------------------
- * Haaf's Game Engine 1.8.1
+ * Haaf's Game Engine 1.9
  * Copyright (C) 2003-2007, Relish Games
  * Maintained 2012-2021 by dmytro.lytovchenko@gmail.com (github @kvakvs)
  * Github -- https://github.com/kvakvs/hge | Discord -- https://discord.gg/TdjamHt
